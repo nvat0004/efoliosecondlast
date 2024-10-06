a16f4fdf053216b4b0720b473669705dccbdeac4
Neeraj Vats <nvat0004@student.monash.edu>
2024-10-06 17:00:53 +1100

    Efloio910

---
+++ efolio910/efolio910/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+//import HelloWorld from './components/HelloWorld.vue'
+import WeatherView from './components/WeatherView.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <WeatherView />
+</template>
